9de0fb9ac646443edcbc5d438c365c751ccea256
ander44440 <ander44440@gmail.com>
2024-01-27 20:19:40 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function generateNumber(){
+
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    if(min >= max) {
+        alert("o segundo VALOR tem que ser maior que o primeiro ")
+    }
+
+      
+        
+    const result = Math.floor(Math.random() * (max - min +1)) + min;
+    alert(result)
+    
+}  
